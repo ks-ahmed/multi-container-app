d3bd85af9d9b5568ad00517c5759e7f9cdeacc63
ks-ahmed <ka-sharif@outlook.com>
2025-05-31 16:52:07 +0100

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import redis 
 
 app = Flask(__name__)
+client = redis.Redis(host='redis', port=6379, decode_responses=True)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, welcome to my multi container application!'
+def welcome():
+    return 'Hello, welcome to my multi container app!'
+
+@app.route('/')
+def count():
+    count = client.incr('hits')
+    return f'This webpage has been viewed {count} time(s)'
 
 if  __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
